c097bf04b5b28a85071b79d4cae63b862feddf32
胡一帆 <yifan.hu@geely.com>
2019-12-10 14:02:21 +0800

    sssss

--- install_quickly.sh
+++ install_quickly.sh
@@ -1,3 +1,4 @@
 
 wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.4.0/kustomize_v3.4.0_linux_amd64.tar.gz
 tar xzvf kustomize_v3.4.0_linux_amd64.tar.gz
+mv kustomize /usr/bin/
